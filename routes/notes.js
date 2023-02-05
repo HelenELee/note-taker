b8ed99c4899e0d4b68fb68af5b2e04aabf7c19c3
HelenELee <HelenELee3@outlook.com>
2023-02-05 11:50:26 +0800

    added notes.html and js

--- routes/notes.js
+++ routes/notes.js
@@ -1,43 +1,42 @@
-const fb = require('express').Router();
+const note = require('express').Router();
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 const uuid = require('../helpers/uuid');
 
-// GET Route for retrieving all the feedback
-fb.get('/', (req, res) => {
-  console.info(`${req.method} request received for feedback`);
+// GET Route for retrieving all the notes
+note.get('/', (req, res) => {
+  console.info(`${req.method} request received for note`);
 
   readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
-// POST Route for submitting feedback
-fb.post('/', (req, res) => {
+// POST Route for submitting note
+note.post('/', (req, res) => {
   // Log that a POST request was received
-  console.info(`${req.method} request received to submit feedback`);
+  console.info(`${req.method} request received to submit note`);
 
   // Destructuring assignment for the items in req.body
-  const { email, feedbackType, feedback } = req.body;
+  const { notetitle, notetext } = req.body;
 
   // If all the required properties are present
-  if (email && feedbackType && feedback) {
+  if (notetitle && notetext) {
     // Variable for the object we will save
-    const newFeedback = {
-      email,
-      feedbackType,
-      feedback,
-      feedback_id: uuid(),
+    const newNote = {
+      notetitle,
+      notetext,
+      note_id_id: uuid(),
     };
 
-    readAndAppend(newFeedback, './db/feedback.json');
+    readAndAppend(newNote, './db/db.json');
 
     const response = {
       status: 'success',
-      body: newFeedback,
+      body: newNote,
     };
 
     res.json(response);
   } else {
-    res.json('Error in posting feedback');
+    res.json('Error in posting note');
   }
 });
 
-module.exports = fb;
+module.exports = note;
